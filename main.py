6a97ea1d4f85f88e335c0d58fcafabc72714440d
Jarret Jeter <jarretjeter@yahoo.com>
2022-05-27 14:24:09 -0700

    add_stats endpoint WIP

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.config["db"] = super_df
 @app.route("/see_stats", methods=["GET"])
 def stats():
     """
-    API query to get a superhero's stats information
+    API GET query to get a superhero's stats information
     """
     global super_df
     name = request.args.get("name", default=None)
@@ -73,7 +73,43 @@ def stats():
         return resp_json, 200, resp_headers
 
     else:
-        return {"status": "error", "msg": "No stats entered"}, 404, {"content-type": "application/json"}
+        result_df = super_df
+        resp_json = {"query": "None, returning database", "result": result_df.to_dict(orient="records")}
+        resp_headers = {"content-type": "application/json"}
+        return resp_json, 200, resp_headers
+
+
+@app.route("/add_stats", methods=["POST"])
+def add_stats():
+    """
+    API POST method to add a superhero to the database
+    """
+    global super_df
+    # name = request.args.get("name", default=None)
+    # superpower = request.args.get("superpower", default=None)
+    # weakness = request.args.get("weakness", default=None)
+
+    try:
+        heroes_added = []
+        heroes_not_added = []
+        data = request.json
+        for superhero in data:
+            if ("name" in superhero) and ("superpower" in superhero) and ("weakness" in superhero):
+                logger.info(f"Adding superhero: {superhero}")
+                super_df.loc[superhero["name"]] = superhero
+                heroes_added.append(superhero)
+            else:
+                logger.info(f"Superhero: {superhero} cannot be added")
+                heroes_not_added.append(superhero)
+        logger.info(f"Added {len(heroes_added)}, did not add {len(heroes_not_added)}")
+        resp_json = {f"Heroes added": f"{len(heroes_added)}",
+        "result": heroes_added,
+        "not added": heroes_not_added}
+        resp_headers = {"content-type": "application/json"}
+        return resp_json, 200, resp_headers
+    except Exception as err:
+        return {"status": "error", "error_msg": str(err)}, 400, {"content-type": "application/json"}
+
 
 
 if __name__ == "__main__":
